6420216bdc6c0f516b4829d93aedf5e73efb7155
oimy <oimqop@gmail.com>
2025-11-19 15:58:15 +0900

    refactoring orbit-engine

---
+++ src/pages/planet/utils/orbits/orbit-processor.ts
@@ -0,0 +1,29 @@
+import type Planet from "../models/planet";
+import type Point from "../models/point";
+
+export interface OrbitProcessor {
+    process(planets: Planet[], accelerations: Point[]): void;
+}
+
+export class SimpleOrbitProcessor implements OrbitProcessor {
+    tick: number;
+
+    constructor(tick: number) {
+        this.tick = tick;
+    }
+    process(planets: Planet[], accelerations: Point[]): void {
+        if (planets.length !== accelerations.length) {
+            throw Error("planets and accelerations lengths are not equal");
+        }
+
+        for (let i = 0; i < planets.length; i++) {
+            planets[i].velocity.x += accelerations[i].x * this.tick;
+            planets[i].velocity.y += accelerations[i].y * this.tick;
+            planets[i].velocity.z += accelerations[i].z * this.tick;
+
+            planets[i].position.x += planets[i].velocity.x * this.tick;
+            planets[i].position.y += planets[i].velocity.y * this.tick;
+            planets[i].position.z += planets[i].velocity.z * this.tick;
+        }
+    }
+}
---
+++ src/pages/planet/utils/orbits/orbit-resetter.ts
@@ -0,0 +1,34 @@
+import type Planet from "../models/planet";
+import { PointFactory } from "../point-factory.utils";
+import type OrbitValidator from "./orbit-validator";
+
+export default interface OrbitResetter {
+    reset(planets: Planet[]): void;
+}
+
+export class SimpleOrbitResetter implements OrbitResetter {
+    validator: OrbitValidator;
+    originalPlanets: Planet[];
+
+    constructor(validator: OrbitValidator, originalPlanets: Planet[]) {
+        this.validator = validator;
+        this.originalPlanets = originalPlanets;
+    }
+
+    private resetOne(planet: Planet): void {
+        if (this.validator.validate(planet)) return;
+
+        const originalPlanet: Planet | undefined = this.originalPlanets.find(
+            (originalPlanet) => planet.id === originalPlanet.id
+        );
+        if (!originalPlanet) return;
+
+        planet.position = PointFactory.copy(originalPlanet.position);
+    }
+
+    reset(planets: Planet[]) {
+        for (let i = 0; i < planets.length; i++) {
+            this.resetOne(planets[i]);
+        }
+    }
+}
